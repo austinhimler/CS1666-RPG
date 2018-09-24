a85aed0bfd38d655e5eb3b3b8ea24109e968d85b
dsg33 <dsg33@pitt.edu>
2018-09-24 14:07:36 -0400

    Added Credits program and makefile

---
+++ Credits.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+// Function declarations
+bool init();
+SDL_Texture* loadImage(std::string fname);
+void close();
+
+// Globals
+SDL_Window* gWindow = nullptr;
+SDL_Renderer* gRenderer = nullptr;
+std::vector<SDL_Texture*> gTex;
+
+bool init() {	
+	// Flag what subsystems to initialize
+	// For now, just video
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+		return false;
+	}
+
+	// Set texture filtering to linear
+	if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+		std::cout << "Warning: Linear texture filtering not enabled!" << std::endl;
+	}
+	
+	
+	gWindow = SDL_CreateWindow("Hello world!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (gWindow == nullptr) {
+		std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		return  false;
+	}
+
+	/* Create a renderer for our window
+	 * Use hardware acceleration (last arg)
+	 * Choose first driver that can provide hardware acceleration
+	 *   (second arg, -1)
+	 */
+	gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+	if (gRenderer == nullptr) {	
+		std::cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		return  false;
+	}
+
+	// Set renderer draw/clear color
+	SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
+			
+	// Initialize PNG loading via SDL_image extension library
+	int imgFlags = IMG_INIT_PNG;
+	int retFlags = IMG_Init(imgFlags);
+	if(retFlags != imgFlags) {
+		std::cout << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << std::endl;
+		return false;
+	}
+	
+	return true;
+}
+
+SDL_Texture* loadImage(std::string fname) {
+	SDL_Texture* newText = nullptr;
+
+	SDL_Surface* startSurf = IMG_Load(fname.c_str());
+	if (startSurf == nullptr) {
+		std::cout << "Unable to load image " << fname << "! SDL Error: " << SDL_GetError() << std::endl;
+		return nullptr;
+	}
+
+	newText = SDL_CreateTextureFromSurface(gRenderer, startSurf);
+	if (newText == nullptr) {
+		std::cout << "Unable to create texture from " << fname << "! SDL Error: " << SDL_GetError() << std::endl;
+	}
+
+	SDL_FreeSurface(startSurf);
+
+	return newText;
+}
+
+void close() {
+	for (auto i : gTex) {
+		SDL_DestroyTexture(i);
+		i = nullptr;
+	}
+
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	gWindow = nullptr;
+	gRenderer = nullptr;
+
+	// Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}
+
+int main() {
+	if (!init()) {
+		std::cout <<  "Failed to initialize!" << std::endl;
+		close();
+		return 1;
+	}
+
+	// Load media
+	gTex.push_back(loadImage("CREDIT_IMG/TestImage.png"));
+	gTex.push_back(loadImage("CREDIT_IMG/TestImage.png"));
+
+	SDL_RenderClear(gRenderer);
+
+	// Render the image
+	SDL_RenderCopy(gRenderer, gTex[0], NULL, NULL);
+	// Display rendering
+	SDL_RenderPresent(gRenderer);
+	// Wait 5 seconds
+	SDL_Delay(5000);
+
+	SDL_RenderCopy(gRenderer, gTex[1], NULL, NULL);
+	SDL_RenderPresent(gRenderer);
+	SDL_Delay(3000);
+
+	close();
+}
---
+++ Makefile
@@ -0,0 +1,13 @@
+CPP_FILES = $(wildcard *.cpp)
+OUT_NAMES = $(patsubst %.cpp,%.o,$(CPP_FILES))
+CC = clang++
+COMPILER_FLAGS = -Wall -I/usr/include/SDL2
+LINKER_FLAGS = -lSDL2 -lSDL2_image -lSDL2_ttf
+
+all: $(OUT_NAMES)
+
+%.o: %.cpp
+	$(CC) $^ $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $@
+
+clean:
+	rm *.o
