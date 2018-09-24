28a788514c3be45f2c72c5ad02424320675e6084
dsg33 <31711485+dsg33@users.noreply.github.com>
2018-09-24 16:08:37 -0400

    Set resolution to 720x720

--- Credits.cpp
+++ Credits.cpp
@@ -4,8 +4,8 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 720;
+const int SCREEN_HEIGHT = 720;
 
 // Function declarations
 bool init();
