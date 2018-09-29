e957752184d7b33ae50ac4e875bafb601e788ecd
Ryan Killen <rek77@pitt.edu>
2018-09-29 12:42:38 -0400

    Added attribute data members

--- Classes/Character.cpp
+++ Classes/Character.cpp
@@ -9,7 +9,15 @@ public:
 	int mpCurrent;
 	int mpMax;
 	std::string imageResource;
-	std::vector<Attribute> attributes;
+
+	//std::vector<Attribute> attributes;
+	//Had array of attributes in github todo but since all will have the same 5, probably better to just list them
+	Strength strength;
+	Intelligence intelligence;
+	Dexterity dexterity;
+	Constitution constitution;
+	Faith faith;
+
 	std::vector<Abilitiy> abilities;
 	//std::vector<Resistance> resistances;
 	//have an array or list each resistance out?
@@ -27,7 +35,11 @@ public:
 	}
 
 	Character(Strength strength, Intelligence intelligence, Dexterity dexterity, Constitution constitution, Faith faith) {
-
+		this->strength = strength;
+		this->intelligence = intelligence;
+		this->dexterity = dexterity;
+		this->constitution = constitution;
+		this->faith = faith;
 		mpCurrent = determineMPMax();
 		hpCurrent = determineHPMax();
 		energyCurrent = determineEnergyMax();
