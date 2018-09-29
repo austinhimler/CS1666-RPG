2263f47227ef0fe54f6e6a671587c5c2aa8be7c1
Ryan Killen <rek77@pitt.edu>
2018-09-29 12:35:58 -0400

    Added functions to Character.cpp and experience to Player.cpp

--- Classes/Character.cpp
+++ Classes/Character.cpp
@@ -6,6 +6,8 @@ public:
 	int hpCurrent;
 	int energyMax;
 	int energyCurrent;
+	int mpCurrent;
+	int mpMax;
 	std::string imageResource;
 	std::vector<Attribute> attributes;
 	std::vector<Abilitiy> abilities;
@@ -16,6 +18,21 @@ public:
 	}
 
 	Character(Strength strength, Intelligence intelligence, Dexterity dexterity, Constitution constitution, Faith faith) {
+
+		mpCurrent = determineMPMax();
+		hpCurrent = determineHPMax();
+		energyCurrent = determineEnergyMax();
+	}
+
+	void determineHPMax() {
+
+	}
+
+	void determineMPMax() {
+
+	}
+
+	void determineEnergyMax{
 
 	}
 };
--- Classes/Player.cpp
+++ Classes/Player.cpp
@@ -1,5 +1,5 @@
 #include <string>
 #include <vector>
 class Player : public Character {
-
+	int currentExperience;
 };
