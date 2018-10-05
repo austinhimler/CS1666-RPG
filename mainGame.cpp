6e33219597c2084c74846f68f3ae5ea3b72fae70
birdmanja <birdmanja@hotmail.com>
2018-10-05 12:32:24 -0400

    Player Class

--- mainGame.cpp
+++ mainGame.cpp
@@ -7,6 +7,7 @@
 #include <SDL_ttf.h>
 #include <cmath>
 #include <fstream>
+#include "Headers/Player.h"
 
 const int SCREEN_WIDTH = 720;
 const int SCREEN_HEIGHT = 720;
@@ -25,52 +26,6 @@ std::vector<SDL_Texture*> gTex;
 Mix_Music *gMusic = NULL;
 Mix_Chunk *gBSound = NULL;
 TTF_Font* font; 
-
-class Player
-{
-public:
-	int strength=1;
-	int intelligence=1;
-	int dexterity=1;
-	int constitution=1;
-	int faith=1;
-	std::string name;
-	//stores stats
-	Player(){}
-	Player(int s, int i, int d, int c, int f, std::string n) 
-	{
-		strength=s;
-		intelligence=i;
-		dexterity=d;
-		constitution=c;
-		faith=f;
-		name=n;
-	}
-	void setAll(int s, int i, int d, int c, int f, std::string n)
-	{
-		strength = s;
-		intelligence = i;
-		dexterity = d;
-		constitution = c;
-		faith = f;
-		name = n;
-		return;
-	}
-	operator std::string() 
-	{ 
-		std::string strengthString = std::to_string(strength);
-		std::string intelligenceString = std::to_string(intelligence);
-		std::string dexterityString = std::to_string(dexterity);
-		std::string constitutionString = std::to_string(constitution);
-		std::string faithString = std::to_string(faith);
-		std::string s = "Name: " + name + " Strength: " + strengthString + " Intelligence: " + intelligenceString + " Dexterity: " + dexterityString + " Constitution: " + constitutionString + " Faith: " + faithString; ;
-		return s;
-	}
-	
-
-private:
-	
-};
 
 class Button {
 private:
@@ -100,7 +55,7 @@ public:
 };
 
 //Player ONE
-Player player1;
+Player player1 = Player::Player(1,1,1,1,1,"nlf4");
 
 bool init() {
 	// Flag what subsystems to initialize
