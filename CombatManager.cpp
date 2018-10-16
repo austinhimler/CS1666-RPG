324cf53fbfa7777a69bbb0475c4509fde7d23798
Jake Himes <jph80@pitt.edu>
2018-10-15 20:56:23 -0400

    status effects

--- CombatManager.cpp
+++ CombatManager.cpp
@@ -69,16 +69,23 @@ CombatManager::~CombatManager()
 
 
 void updateStatus(Character& c) {
-	/**
-	*	Turn - pre-turn:
-						pre-turn effect calculation (if we wanna add status effects that carry over from one battle to the next we can but for now let's just make all effects wiped at the end of combat)
-						check survival
-						start in-turn
-						*/
+	//First check if character is dead
 	if (c.getHPCur() <= 0) {
 		gameOn = false;
 	}
-	// Game on!
+	// Next check the ailments of given character
+	else {
+		if (ailments.size() != 0) {
+			for (int i = 0; i < ailments.size(); i++) {
+				if (ailments[i] == 0) {
+					silenced(c);
+				}
+				else if (ailments[i] == 1) {
+					poisoned(c);
+				}
+			}
+		}
+	}
 }
 void takeAction(Character& c) {
 	/*
@@ -94,6 +101,14 @@ void takeAction(Character& c) {
 
 
 }
+void silenced(Character& c) {
+	// silence effects
+}
+
+void poisoned(Character& c) {
+	//poison effects
+}
+
 bool gameOn = true;
 void combatManager(Player& p) 
 {
@@ -111,6 +126,7 @@ void combatManager(Player& p)
 	// Set up a Character array and populate it (not sorted by dex) 
 	//Character participants[2];
 	vector<Character> participants;
+	vector<int> ailments;
 	participants[0] = Character(p); 
 	participants[1] = Character(e1);
 	// Create QueueManager obj which contains sorting of participant array. 
