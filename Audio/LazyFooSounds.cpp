8a380bbed18a1200e51de57d5caf79ca46773787
austinhimler <42745527+austinhimler@users.noreply.github.com>
2018-10-16 21:52:23 -0400

    Update LazyFooSounds.cpp

--- Audio/LazyFooSounds.cpp
+++ Audio/LazyFooSounds.cpp
@@ -1,6 +1,3 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2015)
-and may not be redistributed without written permission.*/
-
 //Using SDL, SDL_image, SDL_ttf, SDL_mixer, standard IO, math, and strings
 #include <SDL.h>
 #include <SDL_image.h>
