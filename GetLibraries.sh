cec6d4027259ce0f790703a8b0978af9a8927eec
dsg33 <31711485+dsg33@users.noreply.github.com>
2018-11-26 19:39:55 -0500

    Update GetLibraries.sh

--- GetLibraries.sh
+++ GetLibraries.sh
@@ -1,1 +1,1 @@
-apt-get install libsdl2-dev libsdl2-ttf-deb libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libglew-dev libgl-dev libsoil-dev
+apt-get install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libglew-dev libgl1-mesa-dev libsoil-dev
